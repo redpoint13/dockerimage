0171619d070b65f797eec067deb0f7d95c875ed7
tony shouse <redpoint13@gmail.com>
2022-12-09 11:43:34 +0000

    remove syntax error--extra keystrokes

--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup up jupyter-lab --allow-root --ServerApp.ip=0.0.0.0 --ServerApp.port=8888 --no-browser --notebook-dir="/app/" --ServerApp.token='abcdefg1234567890' --ServerApp.password='abcdefg1234567890' > /dev/null 2>&1 &
+nohup jupyter-lab --allow-root --ServerApp.ip=0.0.0.0 --ServerApp.port=8888 --no-browser --notebook-dir="/app/" --ServerApp.token='abcdefg1234567890' --ServerApp.password='abcdefg1234567890' > /dev/null 2>&1 &
 # CMD ["jupyter-lab", "--allow-root", "--notebook-dir=/mnt/ssd0", "--ip=*", "--port=8888", "--no-browser", "&"]
 
 echo "JupyterLab server started."
